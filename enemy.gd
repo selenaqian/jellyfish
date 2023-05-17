b7cd842654395ba80068fc7662e2871e1de5f348
Selena Qian <sqian@pbs.org>
2023-05-17 12:26:46 -0400

    add movement toward player

--- enemy.gd
+++ enemy.gd
@@ -1,6 +1,14 @@
 extends RigidBody2D
 
 
+signal squashed
+
+# Minimum speed of the mob in meters per second.
+@export var min_speed = 150
+# Maximum speed of the mob in meters per second.
+@export var max_speed = 250
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,3 +21,27 @@ func _process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+# This function will be called from the Main scene.
+func initialize(start_position, player_position):
+	position = start_position
+	look_at(player_position)
+	
+	# Rotate this mob randomly within range of -90 and +90 degrees,
+	# so that it doesn't move directly towards the player.
+	rotate(randf_range(-PI / 4, PI / 4))
+
+	var velocity = Vector2(randi_range(min_speed, max_speed), 0.0)
+
+	linear_velocity = velocity.rotated(rotation)
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
+
+
+func squash():
+	squashed.emit()
+	queue_free()
+
